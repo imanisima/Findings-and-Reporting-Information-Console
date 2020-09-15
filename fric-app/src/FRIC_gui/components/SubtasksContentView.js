08b37fb084e414b5a6d398f82a38b63f3e72eb1e
MarcoBroto <msoto9940@gmail.com>
2020-09-15 14:06:18 -0600

    Recreated table with material-ui
    + Table functions properly
    
    * Tableeeds to be broken down into heading and toolbar components
    * Detail view still needs to be connected

--- fric-app/src/FRIC_gui/components/SubtasksContentView.js
+++ fric-app/src/FRIC_gui/components/SubtasksContentView.js
@@ -2,23 +2,14 @@
  *
  */
 
-import React, {Component} from 'react'
-import SubtasksOverviewTable from './SubtasksOverviewTable'
+import React from 'react'
 import SubtaskDetailView from './SubtaskDetailView'
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
-import Badge from 'react-bootstrap/Badge'
 import styles from '../css/SubtasksContentView.module.css'
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'
+import SubtasksOverviewTable from './SubtasksOverviewTable'
 
-// TODO: Testing data, delete when file is connected to production system
-let data = [ 
-	{ id: 4243, title: 'title1', task: 'task5', analyst: 'DC', progress: 34, findings: '', dueDate: new Date() },
-	{ id: 77445, title: 'title2', task: 'task4', analyst: 'MJ', progress: 21, findings: '', dueDate: new Date() },
-	{ id: 344, title: 'title3', task: 'task3', analyst: 'II', progress: 66, findings: '', dueDate: new Date() },
-	{ id: 2, title: 'title4', task: 'task2', analyst: '', progress: 78, findings: '', dueDate: new Date() },
-	{ id: 977, title: 'title5', task: 'task1', analyst: 'MS', progress: 100, findings: '', dueDate: new Date() },
-]
+
+/* TODO: Testing data, delete when file is connected to production system */
+// Available options for detail view
 let analysts = ['MS', 'NO', 'SD', 'WE', 'VD', 'WO']
 let collabs = ['MS', 'NO', 'SD', 'WE', 'VD', 'WO']
 let tasks = ['task1', 'task2', 'task3', 'task4']
@@ -28,48 +19,34 @@ let taskObject = {
 	title: 'Task Test Object',
 	description: 'Voluptate anim non amet cupidatat ut pariatur amet deserunt do irure incididunt nostrud.',
 	progress: 99,
-	dueDate: new Date()
+	task: 'Aliquip sit duis veniam qui.',
+	subtask: 'Nulla nisi consectetur ipsum ex proident.',
+	dueDate: new Date(),
 }
-// End Testing Data
 
-export default class SubtasksContentView extends Component {
-	render() {
-		return (
-			<div className="sytles.subtaskContentView">
-				{/* Subtasks Table View Card */}
-				<Card className={styles.subtaskTableCard}>
-					<Card.Header>
-						<div className={styles.menuButtonContainer}>
-							<Button className={styles.menuButton} variant="success">Archive</Button>
-							<Button className={styles.menuButton} variant="primary">Promote</Button>
-							<Button className={styles.menuButton} variant="primary">Save</Button>
-							<Button className={styles.menuButton} variant="danger">Cancel</Button>
-						</div>
-						
-						<div className={styles.addButtonContainer}>
-							<Button className={styles.addButton} variant="primary">New <AddCircleOutlineIcon className={styles.alignCenter} fontSize="small" /></Button>
-						</div>
-					</Card.Header>
+// Data passed into overview table
+let data = [ 
+	{ id: 4243, title: 'title1', task: 'task5', analyst: 'DC', progress: 34, findings: 'Occaecat nostrud fugiat ex ad elit deserunt mollit adipisicing occaecat esse dolore occaecat.', dueDate: new Date() },
+	{ id: 77445, title: 'title2', task: 'task4', analyst: 'MJ', progress: 21, findings: 'Est cupidatat voluptate cupidatat id sit commodo mollit exercitation pariatur tempor eiusmod.', dueDate: new Date() },
+	{ id: 344, title: 'title3', task: 'task3', analyst: 'II', progress: 66, findings: 'Fugiat veniam deserunt cillum labore consectetur tempor elit esse nisi.', dueDate: new Date() },
+	{ id: 2, title: 'title4', task: 'task2', analyst: '', progress: 78, findings: 'Pariatur dolore cupidatat voluptate cupidatat minim.', dueDate: new Date() },
+	{ id: 977, title: 'title5', task: 'task1', analyst: 'MS', progress: 100, findings: 'Magna nulla adipisicing Lorem sint proident elit laborum.', dueDate: new Date() },
+]
+const headings = [
+	{ id: 'id', numeric: true, disablePadding: true, label: '_id' },
+	{ id: 'title', numeric: false, disablePadding: false, label: 'Title' },
+	{ id: 'task', numeric: false, disablePadding: false, label: 'Task' },
+	{ id: 'analyst', numeric: false, disablePadding: true, label: 'Analyst' },
+	{ id: 'progress', numeric: false, disablePadding: false, label: 'Progress' },
+	{ id: 'findings', numeric: false, disablePadding: false, label: 'Findings' },
+	{ id: 'dueDate', numeric: false, disablePadding: true, label: 'Due Date' },
+];
+/* End Testing Data */
 
-					<Card.Body>
-						<SubtasksOverviewTable data={data} />
-					</Card.Body>
-				</Card>
-
-				{/* Subtask Detail View Card */}
-				<Card className={styles.subtaskDetailCard}>
-					<Card.Body>
-						<SubtaskDetailView 
-							selected={taskObject}
-							progressOptions={progOptions}
-							analystOptions={analysts}
-							collabOptions={collabs}
-							taskOptions={tasks}
-							subtaskOptions={subtasks}
-						/>
-					</Card.Body>
-				</Card>
-			</div>
-		)
-	}
+export default function SubtasksContentView() {
+	return (
+		<container className="sytles.subtaskContentView">
+			<SubtasksOverviewTable rows={data} headings={headings}/>
+		</container>
+	)
 }
--- fric-app/src/FRIC_gui/components/SubtasksOverviewTable.js
+++ fric-app/src/FRIC_gui/components/SubtasksOverviewTable.js
@@ -2,40 +2,370 @@
  *
  */
 
-import React, {Component} from 'react'
-import Table from 'react-bootstrap/Table'
-import Form from 'react-bootstrap/Form'
-import SubtasksOverviewTableRow from './SubtasksOverviewTableRow'
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, darken, makeStyles, withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import EditIcon from '@material-ui/icons/Edit';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 
-export default class SubtasksOverviewTable extends Component {
-	render() {
-		return (
-			<div>
-				<Table striped bordered responsive size="sm" variant="dark">
-					<thead>
-						<th><Form.Check custom id="selectAll" type="checkbox" label/></th>
-						<th>Title</th>
-						<th>Task</th>
-						<th>Analyst</th>
-						<th>Progress</th>
-						<th>Findings</th>
-						<th>Due Date</th>
-					</thead>
-					<tbody>
-						{this.props.data.map((item) => {
-							return <SubtasksOverviewTableRow 
-							id={item.id} 
-							title={item.title} 
-							task={item.task} 
-							analyst={item.analyst} 
-							progress={item.progress} 
-							findings={item.findings} 
-							dueDate={item.dueDate} />}
-						)}
-					</tbody>
-				</Table>
-			</div>
-		)
-	}
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
 }
 
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const useToolbarStyles = makeStyles((theme) => ({
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+  },
+  highlight:
+    theme.palette.type === 'light'
+			? {
+					color: theme.palette.common.white,
+					backgroundColor: lighten("#066ff9", 0.1) //lighten(theme.palette.primary.dark, 0.20) 
+				}
+			: {
+					color: theme.palette.text.primary,
+					backgroundColor: theme.palette.primary.light,
+				},
+  title: {
+    flex: '1 1 100%',
+  },
+}));
+
+const AddButton = withStyles((theme) => ({
+  root: {
+    color: theme.palette.common.white,
+    backgroundColor: "#066ff9",
+    '&:hover': {
+      backgroundColor: darken("#066ff9", 0.13),
+    },
+  },
+}))(Button);
+
+const CustomTableToolbar = (props) => {
+  const classes = useToolbarStyles();
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      className={clsx(classes.root, {
+        [classes.highlight]: numSelected > 0,
+      })}
+    >
+      {numSelected > 0 ? (
+        <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <container className={classes.title}>
+					<AddButton variant="contained" color="#066ff9" startIcon={<AddIcon />}>
+						New
+					</AddButton>
+				</container>
+      )}
+
+      {numSelected > 0 ? (
+				<Tooltip title="Edit">
+					<IconButton aria-label="edit" style={{color: "dark-grey", backgroundColor: "#ffffff"}}><EditIcon  /></IconButton>
+				</Tooltip>
+				// <Button variant="contained" endIcon={<EditIcon />}>Edit</Button>
+			) : (
+				<Tooltip title="Filter list">
+					<Button variant="contained" endIcon={<FilterListIcon />}>Filter</Button>
+				</Tooltip>
+			)}
+    </Toolbar>
+  );
+};
+
+CustomTableToolbar.propTypes = { numSelected: PropTypes.number.isRequired, };
+
+function CustomTableHead(props) {
+  const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+						inputProps={{ 'aria-label': 'select all desserts' }}
+						style={{color: "#066ff9"}}
+          />
+        </TableCell>
+        {props.headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+CustomTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  paper: {
+    width: '100%',
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    minWidth: 750,
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+}));
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+		},
+		"&$hover:hover": {
+      backgroundColor: lighten("#066ff9",0.85) //lighten(theme.palette.primary.light,0.85)
+		},
+		"&$selected, &$selected:hover": {
+			backgroundColor: lighten("#066ff9",0.75) //lighten(theme.palette.primary.dark, 0.70)
+		},
+	},
+	hover: {},
+	selected: {},
+}))(TableRow);
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+		color: theme.palette.common.white,
+  },
+  body: {
+		fontSize: 14,
+	},
+}))(TableCell);
+
+export default function SubtasksOverviewTable(props) {
+  const classes = useStyles();
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('calories');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(true);
+  const [rowsPerPage, setRowsPerPage] = React.useState(11);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = props.rows.map((n) => n.id);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => { setPage(newPage); };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => { setDense(event.target.checked); };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, props.rows.length - page * rowsPerPage);
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <CustomTableToolbar numSelected={selected.length} />
+        <TableContainer>
+          <Table
+            className={classes.table}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+            aria-label="custom table"
+          >
+            <CustomTableHead
+							headCells={props.headings}
+              classes={classes}
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={props.rows.length}
+            />
+            <TableBody>
+              {stableSort(props.rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.id);
+                  const labelId = `custom-table-checkbox-${index}`;
+
+                  return (
+                    <StyledTableRow
+                      hover
+                      onClick={(event) => handleClick(event, row.id)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.id}
+                      selected={isItemSelected}
+                    >
+                      <StyledTableCell padding="checkbox">
+                        <Checkbox
+                          checked={isItemSelected}
+													inputProps={{ 'aria-labelledby': labelId }}
+													style={{color: "#066ff9"}}
+                        />
+                      </StyledTableCell>
+                      <StyledTableCell component="th" id={labelId} align="right" scope="row" padding="none">
+                        {row.id}
+                      </StyledTableCell>
+                      <StyledTableCell align="left">{row.title}</StyledTableCell>
+                      <StyledTableCell align="left">{row.task}</StyledTableCell>
+                      <StyledTableCell align="left" padding="none">{row.analyst}</StyledTableCell>
+                      <StyledTableCell align="right" padding="none">{row.progress}</StyledTableCell>
+                      <StyledTableCell align="left" >{row.findings}</StyledTableCell>
+                      <StyledTableCell align="left" padding="none">{row.dueDate.toLocaleString()}</StyledTableCell>
+                    </StyledTableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10]}
+          component="div"
+          count={props.rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+      {/* <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      /> */}
+    </div>
+  );
+}
+
+SubtasksOverviewTable.propTypes = {
+	rows: PropTypes.object.isRequired,
+	headings: PropTypes.object.isRequired,
+}
