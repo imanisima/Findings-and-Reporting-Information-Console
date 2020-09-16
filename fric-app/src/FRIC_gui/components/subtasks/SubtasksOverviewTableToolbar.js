f52b53c105ed7c8d83b7effa54f75dc605c8f18c
Marco Soto <msoto9940@gmail.com>
2020-09-16 17:43:38 -0600

    Subtasks Page (#5)
    * Created SetupContentView component
    
    Includes index file for testing components
    
    * Moved view to component directory
    
    * Created skeletons of SubtasksView
    
    + Includes .gitignore for node_modules
    + SubtasksView.js is broken down into the following subview components:
    OverviewTableRowView.js
    SubtaskDetailView.js
    OverviewTable.js
    
    * File names can be changed later if necessary.
    
    * Update to nested table view
    
    + includes working table and data rendering for table cells
    + Linked to subtasks page but the component can be nested in another component if necessary
    
    * Rendered content view almost complete
    
    + Renamed parent component SubtasksContentView
    
    * Needs spatial styling for subtask table and detail views
    * Needs mutliselector and file selector implementation in subtask detail view
    
    Bug: Date picker in subtask detail view does not render correctly
    
    * Recreated table with material-ui
    
    + Table functions properly
    
    * Tableeeds to be broken down into heading and toolbar components
    * Detail view still needs to be connected
    
    * Split table into multiple component files
    
    + Table, toolbar, and head are in their seperate files now
    + Removed unused table files
    
    * Subtasks detail view skeleton complete
    
    + Detail view section skeleton completed, ready for system connection
    
    * Needs to be styled with table section on next commit
    
    * Styled content page, removed unused css files
    
    + Styled content view page to show both table and detail views
    + Added missing buttons to table view
    
    - Removed unused css files
    - Removed unused imports
    
    * package json updates
    
    + Includes missing modules for package-lock.json
    
    - Excludes devDependencies from package.json
    
    * Linked subtasks page
    
    * Moved files to seperate page folders
    
    * Removed unused files

---
+++ fric-app/src/FRIC_gui/components/subtasks/SubtasksOverviewTableToolbar.js
@@ -0,0 +1,83 @@
+/**
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, darken, makeStyles, withStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import EditIcon from '@material-ui/icons/Edit';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+
+const useToolbarStyles = makeStyles((theme) => ({
+	root: {
+	paddingLeft: theme.spacing(2),
+	paddingRight: theme.spacing(1),
+	},
+	highlight:
+	theme.palette.type === 'light'
+			? {
+					color: theme.palette.common.white,
+					backgroundColor: lighten("#066ff9", 0.1) //lighten(theme.palette.primary.dark, 0.20) 
+				}
+			: {
+					color: theme.palette.text.primary,
+					backgroundColor: theme.palette.primary.light,
+				},
+	title: {
+	flex: '1 1 100%',
+	},
+}));
+
+const AddButton = withStyles((theme) => ({
+	root: {
+	color: theme.palette.common.white,
+	backgroundColor: "#066ff9",
+	'&:hover': {
+		backgroundColor: darken("#066ff9", 0.13),
+	},
+	},
+}))(Button);
+
+export default function SubtasksOverviewTableToolbar(props) {
+	const classes = useToolbarStyles();
+	const { numSelected } = props;
+
+	return (
+		<Toolbar
+			className={clsx(classes.root, {
+			[classes.highlight]: numSelected > 0,
+			})}
+		>
+		{numSelected > 0 ? (
+			<Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
+				{numSelected} selected
+			</Typography>
+			) : (
+			<container className={classes.title}>
+				<AddButton variant="contained" color="#066ff9" startIcon={<AddIcon />}>
+					New
+				</AddButton>
+			</container>
+		)}
+		{numSelected > 0 ? (
+			<Tooltip title="Edit">
+				<IconButton aria-label="edit" style={{color: "dark-grey", backgroundColor: "#ffffff"}}><EditIcon  /></IconButton>
+			</Tooltip>
+			// <Button variant="contained" endIcon={<EditIcon />}>Edit</Button>
+			) : (
+			<Tooltip title="Filter list">
+				<Button variant="contained" endIcon={<FilterListIcon />}>Filter</Button>
+			</Tooltip>
+		)}
+	</Toolbar>
+	);
+};
+
+SubtasksOverviewTableToolbar.propTypes = { numSelected: PropTypes.number.isRequired, };
