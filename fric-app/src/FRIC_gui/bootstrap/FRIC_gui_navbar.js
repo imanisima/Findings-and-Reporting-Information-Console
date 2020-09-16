f97fb845a990021c9997c80c09b21f239c6e94bb
Imani A <imartin@miners.utep.edu>
2020-09-16 18:42:32 -0500

    navbar links fixed

--- fric-app/src/FRIC_gui/bootstrap/FRIC_gui_navbar.js
+++ fric-app/src/FRIC_gui/bootstrap/FRIC_gui_navbar.js
@@ -8,24 +8,24 @@ class MainNav extends Component{
       <div >
 
           <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
-            <Navbar.Brand href="#home">F.R.I.C.</Navbar.Brand>
+            <Navbar.Brand href="/main">F.R.I.C.</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto">
                 <NavDropdown title="View" id="collasible-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Tasks</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Archives</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Systems</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Summary</NavDropdown.Item>
+                  <NavDropdown.Item href="/tasks/new">Tasks</NavDropdown.Item>
+                  <NavDropdown.Item href="/archive">Archives</NavDropdown.Item>
+                  <NavDropdown.Item href="/systems/new">Systems</NavDropdown.Item>
+                  <NavDropdown.Item href="/summary">Summary</NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link href="#events">Events</Nav.Link>
+                <Nav.Link href="/event">Events</Nav.Link>
               </Nav>
               <Nav>
-              <Nav.Link href="#notification">Notifications</Nav.Link>
+              <Nav.Link href="/notification">Notifications</Nav.Link>
                 <NavDropdown title="Settings" id="collasible-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Configurations</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Settings</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Help</NavDropdown.Item>
+                  <NavDropdown.Item href="/configuration">Configurations</NavDropdown.Item>
+                  <NavDropdown.Item href="/setup">Settings</NavDropdown.Item>
+                  <NavDropdown.Item href="#help">Help</NavDropdown.Item>
                   {/* <NavDropdown.Divider /> */}
                   {/* <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
                 </NavDropdown>
