31af72117aadc07b0f89ee2fd97ed49b7ada44ff
Marco Soto <msoto9940@gmail.com>
2020-09-28 19:11:32 -0600

    New system template, frontend layout and project structure changes (#12)
    * Removed node_modules awith changes to root gitignore
    
    * Removed root node_modules
    
    * Fixed styling bug with buttons
    - Changed button class styling in archive component css files.
    
    * Subtask page layout and styling changes
    + Moved save and cancel buttons to detail view as requested by client feedback.
    - Changed type of Subtasks page to function
    - removed unused files
    
    * Moved Event Tree components to dedicated folder.
    + Updated all files and imports that used this component.
    
    * Major Frontend Commit, Structure and Files
    - Removed unused files
    + Changed structure of some components from react classes to react functions
    + Fixed routing issues and start page through package.json
    - Changed react app mount name to 'fric-app'
    + Wrapped routing in function
    - Removed unnecessary code throughout
    
    * Added contributors to package.json
    - Removed unused .vscode files
    + Added to .gitignore
    + Copied readme to fric-app directory
    
    * Fixed package.json bug
    
    * New iteration of events page
    + Includes new events page with new layout
    + Minor changes to subtasks page
    
    * Layout Experimentation, new navbar and homepage layout
    + new layout included in PageLayout.js
    
    * Bug in last commit, extracted buttons from detail view to PageLayout.js
    
    * Created generic layout skeleton for all pages, inlcudes navigation bars and detail views
    + Generic skeleton view now works for embedded views
    + Moved test data to separate folder and files, * Delete when connected to backend
    + Altered prop types of some components
    + Added dark theme, included in ThemeColors.js
    + Added test page to router for custom testing, * Delete when deploying production build
    - Removed unused imports
    - Removed unused files
    
    * Prep for summary page components
    + Includes component for displaying project summary in card form
    
    * Switched main page layout
    
    * Nested page components in new layout component
    + Renamed files
    + Renamed event folder to events
    + Renamed system folder to systems
    - Removed unused routes
    - Removed notifications page (is nested in navbar now)
    * Some components still need to be connected (ex. detail views for editing some items)

--- fric-app/src/components/setup/SetupForm.js
+++ fric-app/src/components/setup/SetupForm.js
@@ -4,122 +4,43 @@
  * Created by Marco Soto
  */
 
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import styles from '../../css/setup/SetupForm.module.css';
+import React, { useState } from 'react'
+import styles from '../../css/setup/SetupContent.module.css'
+import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-import axios from 'axios';
 
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import DialogContent from '@material-ui/core/DialogContent';
-
-export default function SetupForm(props) {
-	const [radioVal, setRadioVal] = useState('new');
-	const [showSync, setShowSync] = useState(false);
-	const [user, setUser] = useState(null);
-	const [choiceInput, setChoiceInput] = useState(null);
-	
-	const handleRadioChange = (event) => {
-		setRadioVal(event.target.value);
-		setShowSync(!showSync)
-	};
-
-	const handleSubmitClick = (e) => {
-		switch (radioVal) {
-			case "new":
-				const defaultEvent = {
-					name: choiceInput,
-					description: "",
-					type: "",
-					version: "1.0",
-					derivedFrom: "",
-					assessed: new Date().toUTCString(),
-					declassified: new Date().toUTCString(),
-					organization: "",
-					securityGuide: "",
-					classification: "",
-					customer: "",
-					archived: false,
-					team: [user]
-				};
-
-				axios.post('http://localhost:5000/events/new', {
-					params: defaultEvent
-				})
-					.then(response => {
-						console.log(response)
-						console.log(response.data);
-						props.submitAction();
-						window.location = '/';
-					})
-					.catch(error => {
-						//TODO: display error message
-						console.log(error)
-					});
-				break;
-			case "sync":
-				//TODO: Grab database from lead analyst
-				//TODO: Store copy of lead analyst database on this system
-				//TODO: display error message if request fails
-				break;
-			default: throw Error;
-		}
-	}
+export default function SetupContentView(props) {
+	const [showingIPForm, showIPForm] = useState(false);
 
 	return (
 		<>
-			<DialogContent id="setupContentForm" className={styles.setupContentForm}>
-				<Typography variant="h6" className={styles.title}>
-					Findings and Reportings Information Console (FRIC)
-				</Typography>
-				<Typography variant="subtitle1" style={{ color: "#ffc108"}} className={styles.subtitle}>No Events Detected in Your Local System</Typography>
-
-				{/* Enter User Initials */}
-				<Form.Group controlId="user">
-					<FormLabel>Please enter your initials:</FormLabel>
-					<Form.Control type="text" placeholder="Enter user initials" onChange={ e => setUser(e.target.value) } />
+			<h5 className={styles.title}>Findings and Reportings Information Console (FRIC)</h5>
+			<Form id="setupContentForm" className={styles.setupContentForm}>
+				<Form.Group controlId="">
+					<Form.Label>There is no existing event in your local system</Form.Label>
+					<Form.Control type="text" placeholder="Enter event" />
 				</Form.Group>
 
-				{/* Radio buttons */}
-				<FormControl component="fieldset">
-					<RadioGroup aria-label="action" name="action" value={radioVal} onChange={handleRadioChange}>
-						<FormControlLabel value="new" control={<Radio color="primary" />} label="Create a New Event" />
-						<FormControlLabel value="sync" control={<Radio color="primary" />} label="Sync with Lead Analyst" />
-					</RadioGroup>
-				</FormControl>
+				<Form.Group class="setupContentButton" controlId="">
+					<Form.Label>Pleaser enter your initials:</Form.Label>
+					<Form.Control type="text" placeholder="Enter initials" />
+				</Form.Group>
+				
+				<Form.Group controlId="">
+					<Form.Check custom id="eventCheckbox" type="checkbox" label="Create as new event" className={styles.eventCheckbox} />
+					<Form.Check custom onClick={() => showIPForm(!showingIPForm)} id="syncCheckbox" type="checkbox" label="First time sync with lead analyst" />
+				</Form.Group>
 
-				{
-					// Reactive radio section
-					(showSync) ? (
-						// Enter lead analyst ip address
-						<Form.Group controlId="sync">
-							<FormLabel>Enter Lead Analyst IP Address</FormLabel>
-							<Form.Control type="text" placeholder="ex. 1.0.0.1" onChange={ e => setChoiceInput(e.target.value) } />
-						</Form.Group>
-					) : (
-						// Enter event name
-						<Form.Group controlId="event">
-							<FormLabel>Enter Event Name</FormLabel>
-							<Form.Control type="text" placeholder="Enter event" onChange={ e => setChoiceInput(e.target.value) } />
-						</Form.Group>
-					)
-				}
+				{showingIPForm && <Form.Group controlId="">
+					<Form.Label>Please enter the lead analysts IP address.</Form.Label>
+					<Form.Control type="text" placeholder="Enter lead analyst IP address." />
+				</Form.Group>}
 
-				{/* Submit Button */}
 				<Form.Group className={styles.center}>
-					<Button onClick={handleSubmitClick} variant="contained" size="large" color="primary">Submit</Button>
+					<Button className={styles.setupContentButton} variant="primary">Submit</Button>
+					<Button onClick={props.onCancel} className={styles.setupContentButton} variant="danger">Cancel</Button>
 				</Form.Group>
-			</DialogContent>
+			</Form>
 		</>
 	);
-}
-
-SetupForm.propTypes = {
-	submitAction: PropTypes.func.isRequired,
 }
