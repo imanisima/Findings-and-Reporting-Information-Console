a1f07fd5ab44ad6130533e0d279505fbc842b650
Erick Nevarez <ericknevarez19922@gmail.com>
2020-10-08 15:25:15 -0600

    GET req

--- fric-app/src/pages/SystemsPage.js
+++ fric-app/src/pages/SystemsPage.js
@@ -2,7 +2,7 @@
  * 
  */
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import LayoutTemplate from '../components/general/LayoutTemplate';
 import SystemsDetail from '../components/systems/FRIC_gui_system_details';
@@ -12,10 +12,12 @@ import axios from 'axios';
 import PropTypes from 'prop-types';
 
 import { options } from '../components/general/test/eventstestdata'; //TODO: remove test data import when connected to backend
-import { getSystems } from '../services/services';
+import Spinner from '../components/general/Spinner';
 
 export default function SystemsPage(props) {
 	const [selectedEvent, setSelectedEvent] = useState(null);
+	const [ isLoading, setIsLoading ] = useState(false)
+    const [ data, setData ] = useState([])
 
 	const headings = [
 		{ id: 'name', numeric: false, disablePadding: false, label: 'System' },
@@ -24,18 +26,39 @@ export default function SystemsPage(props) {
 		{ id: 'progress', numeric: false, disablePadding: false, label: 'Progress' },
 	];
 
-	const data = [
+	const data2 = [
 		{ name: 'System1', numTasks: 2, numFindings: 3, progress: 'Not Applicable', },
 	]
 
-	const test = getSystems();
-	console.log(test)
+	useEffect(() => {
+		async function getSystems() {
+			setIsLoading(true)
+			try {
+				const fetch = await axios.get('http://localhost:5000/systems/')
+				const response = await fetch.data
+				setData(response)	
+			}
+			catch (error) {
+				console.log(error)
+			} finally {
+				setIsLoading(false)
+			}
+		}
+		getSystems()
+	}, [])
 
+	if( data.length > 0 ) {
+		console.log(data[0].SystemName)
+	}
 	return (
 		// Added dark theme provider, remove for normal colors
 		<ThemeProvider theme={darkTheme}>
 			<LayoutTemplate
-				mainContentComponent={<SystemOverviewTable rows={data} headings={headings} setDetailData={setSelectedEvent} />}
+				mainContentComponent={
+					(isLoading) ? <Spinner /> : (
+						(data != null && data.length > 0) ? <SystemOverviewTable rows={data2} headings={headings} setDetailData={setSelectedEvent} /> : <></>
+					)
+				}
 				detailComponent={<SystemsDetail />}
 			/>
 		</ThemeProvider>
--- fric-app/src/services/services.js
+++ fric-app/src/services/services.js
@@ -4,18 +4,7 @@ const postEvent = function() {
 
 }
 
-export async function getSystems() {
-    try{
-        let res = await axios.get('http://localhost:5000/systems/')
-            .then(res => {
-                console.log(res.data)
-                return res.data
-    })
-    } catch(err) {
-        console.log(err.res)
-        return []
-    };
-}
+
 
 /*export const getEvent = async () => {
     await axios.get('http://localhost:5000/events/')
