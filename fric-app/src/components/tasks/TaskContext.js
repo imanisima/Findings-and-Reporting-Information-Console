4465d0d1baeee1fb0cb019724f9e73e4ce1dcf8d
Marco Soto <msoto9940@gmail.com>
2020-10-14 04:51:14 -0600

    Reimplemented task detail view with context data. Added TaskForm as it's child.
    + Added aggregation match stage in tasks route; ignores archived tasks in tasks table view.
    + Removed click out to close behavior of NewTaskDialog window.
    + Added new contexts to TaskContext
    + Edit task now reloads tasks table on success

--- fric-app/src/components/tasks/TaskContext.js
+++ fric-app/src/components/tasks/TaskContext.js
@@ -15,4 +15,9 @@ export const TaskRelatedTasksContext = createContext(null);
 export const TaskAnalystsContext = createContext(null);
 export const TaskCollaboratorsContext = createContext(null);
 export const TaskDueDateContext = createContext(null);
+<<<<<<< HEAD
 >>>>>>> 72406b3a... Created react contexts to pass states to nested task components. Implemented NewTaskDialog window.
+=======
+export const TaskAttachmentContext = createContext(null);
+export const TaskArchivedContext = createContext(null);
+>>>>>>> 61b3428c... Reimplemented task detail view with context data. Added TaskForm as it's child.
