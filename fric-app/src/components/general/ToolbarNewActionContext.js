06505bd6dcbf1b7d30909e081382103fd6a485b1
Marco Soto <msoto9940@gmail.com>
2020-10-21 10:39:27 -0600

    Tasks page functionality (#16)
    * Minor change, includes task page test data file
    
    + Added key to analyst Menu in sync form
    - Removed color tag in SyncForm
    
    * Worked on passing selected task object to detail view for editing. Work in progress.
    
    + Task centered detail and overview table views
    + Changed custom table component props
    + Slight multiselect component changes; * needs to handle changes of selected options
    
    * Connected database to tasks page. Able to edit existing items but not save edits.
    
    + Added tasks request to backend router
    + Fixed mongoose model for Task
    + Added request handling to task request router
    + Handled passing selected task to detail view by passing the objects _id
    + Requested selected task by id in TaskDetailView
    + Created reload() function in TasksPage to handle table data fetching
    
    * Needs to handle mongoose Date casting
    * Needs input validation for detail view
    * Needs post (or put) request to save edited task
    
    * Completed edit task backend functionality. Still needs frontend validation.
    
    + Modified edit task function to PUT request
    + Made all request in tasks router async
    + Cleaned handleSaveClick request in TaskDetailView
    + Added enum requirement to Task mongoose model
    + Added EnumeratedTypes file to shared folder in backend; used for easy access to available values throughout the backend.
    
    * Moved nodemon to devDependencies in package.json
    
    * Removed unused code and unused files.
    
    * Extracted shared methods and enums to 'shared' folder. Removed unused files.
    
    + Created 'shared' folder in src dir that should hold all files that are used throughout the frontend
    - Removed unused Task css modules
    
    * Extracted Task form to TaskForm component. Created dialog box for creating new tasks.
    
    + Includes NewTaskDialog css module
    * TODO: Values in TaskForm need to grab data from contexts
    
    * Created react contexts to pass states to nested task components. Implemented NewTaskDialog window.
    
    + Changed method of new task route
    + Added context to CustomTableToolbar to handle 'new' button click
    + Passed main and detail components with detail view handler contexts. * Could not function with cloning object.
    + Updated NewTaskDialog to nest TaskForm and handle submit requests.
    + Added context values to TaskForm to update when changed. Implemented to reuse TaskForm
    + Added detail view handlers from context in TasksOverviewTable
    + Mounted NewTaskDialog to tasks page.
    
    * TODO: fix multiselect props and state; selected values should match prop values passed in and set them.
    * TODO: handler frontend form validation with ui in TaskForm
    * TODO: add attachment field to TaskForm
    * TODO: Implement archive tasks request
    * TODO: if possible, fix comparators for enumerated values in tasks table
    
    * Reimplemented task detail view with context data. Added TaskForm as it's child.
    
    + Added aggregation match stage in tasks route; ignores archived tasks in tasks table view.
    + Removed click out to close behavior of NewTaskDialog window.
    + Added new contexts to TaskContext
    + Edit task now reloads tasks table on success
    
    * Implemented archive task feature. Cna
    
    + Task router now supports archiving multiple tasks
    + Task table now requests data from /tasks/table route
    + Tasks page now supports archiving tasks feature
    + Included archive tasks confirmation dialog window
    - Removed unused code
    
    * Set system detail view to receive close detail action from context instead of props.
    
    * Removed unused PageLayout file
    
    * Reduced context nesting when passing in task form field values and setters.
    
    + Passed task form values and setters through one context object. This reduces the context nesting previously implemented.
    - Removed unused contexts from TaskContext.js
    
    * Minor functional changes
    
    * TaskForm cosmetic changes

---
+++ fric-app/src/components/general/ToolbarNewActionContext.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+
+import { createContext } from 'react';
+
+export const ToolbarNewActionContext = createContext(null);
---
+++ fric-app/src/components/tasks/TaskDetailView.js
@@ -0,0 +1,144 @@
+/*
+ *
+ */
+
+import React, { useState, useEffect, useContext, useMemo } from 'react'
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import CancelIcon from '@material-ui/icons/Cancel';
+import HelpOutlineRoundedIcon from '@material-ui/icons/HelpOutlineRounded'
+import Form from 'react-bootstrap/Form'
+
+import Spinner from '../general/Spinner';
+import TaskForm from './TaskForm';
+import { TaskContext } from './TaskContext';
+import { DetailViewActionContext } from '../general/LayoutTemplate';
+
+export default function TaskDetailView(props) {
+	const [contentIsLoading, setContentIsLoading] = useState(true);
+	const [name, setName] = useState('');
+	const [description, setDescription] = useState('');
+	const [progress, setProgress] = useState('');
+	const [priority, setPriority] = useState('');
+	const [relatedTasks, setRelatedTasks] = useState([]);
+	const [analysts, setAnalysts] = useState([]);
+	const [collabs, setCollabs] = useState([]);
+	const [dueDate, setDueDate] = useState(new Date());
+	const [attachment, setAttachment] = useState('');
+	const [archived, setArchived] = useState(false);
+	const taskProviderValue = useMemo(() => ({
+		name, setName,
+		description, setDescription,
+		progress, setProgress,
+		priority, setPriority,
+		relatedTasks, setRelatedTasks,
+		analysts, setAnalysts,
+		collabs, setCollabs,
+		attachment, setAttachment,
+		dueDate, setDueDate,
+		archived, setArchived
+	}), [name, description, progress, priority, relatedTasks,
+		analysts, collabs, attachment, dueDate, archived]);
+	const closeDetailAction = useContext(DetailViewActionContext);
+	
+	const handleSaveClick = () => {
+		axios.put("http://localhost:5000/tasks/update", {
+			params: {
+				id: (props.selectedTask != null && props.selectedTask.length === 1) ? props.selectedTask[0] : '',
+				name: name,
+				description: description,
+				progress: progress,
+				priority: priority,
+				associations: relatedTasks,
+				analysts: analysts,
+				collaborators: collabs,
+				dueDate: dueDate.toUTCString(), // Must be converted to value that can be sent in request body
+				attachment: attachment,
+				archived: archived, // New elements will never be archived
+			}
+		})
+			.then(res => {
+				console.log(res);
+				setContentIsLoading(true); // Reset to spinner for next edit request
+				props.reload(); // Reload table content
+				closeDetailAction(); // Close detail view tray
+			})
+			.catch(err => {
+				console.log(err);
+				//TODO: display error message
+			})
+	};
+
+	useEffect(() => {
+		if (props.selectedTask != null && props.selectedTask.length === 1) {
+			axios.get('http://localhost:5000/tasks', {
+				params: {
+					id: props.selectedTask[0]
+				}
+			})
+				.then(res => {
+					console.log(res.data);
+					//TODO: validate request data before setting values
+					setName(res.data.name);
+					setDescription(res.data.description);
+					setDueDate(new Date(res.data.dueDate));
+					setPriority(res.data.priority);
+					setProgress(res.data.progress);
+					setAnalysts(res.data.analysts);
+					setCollabs(res.data.collaborators);
+					setRelatedTasks(res.data.associations);
+					setContentIsLoading(false);
+				})
+				.catch(err => {
+					console.log(err);
+					//TODO: display error message
+					setContentIsLoading(false);
+				})
+		}
+	}, [props.selectedTask]);
+
+	return (
+		<>
+			{
+				(contentIsLoading) ? <Spinner /> : (
+					<>
+						<div style={{ textAlign: "center" }}>
+							<h4 style={{ display: "inline-block", padding: "0.3em" }}>Task Detail View</h4>
+							<HelpOutlineRoundedIcon size="large" style={{ verticalAlign: "middle" }} />
+						</div>
+						{/* <Button variant="light"><HelpOutlineRoundedIcon /></Button> */}
+
+						<TaskContext.Provider value={taskProviderValue}>
+							<TaskForm />
+						</TaskContext.Provider>
+
+						{/* Action Buttons */}
+						<Form.Group>
+							<Button
+								onClick={handleSaveClick}
+								variant="contained"
+								size="large"
+								startIcon={<SaveIcon />}
+								style={{ backgroundColor: "#ffc108", color: "charcoal", margin: "0.5em", }}
+							>Save</Button>
+							<Button
+								onClick={closeDetailAction}
+								variant="contained"
+								size="large"
+								startIcon={<CancelIcon />}
+								style={{ backgroundColor: "#dc3545", color: "white", margin: "0.5em", }}
+							>Cancel</Button>
+						</Form.Group>
+					</>
+				)
+			}
+		</>
+	);
+}
+
+TaskDetailView.propTypes = {
+	selectedTask: PropTypes.array.isRequired,
+	reload: PropTypes.func.isRequired,
+}
