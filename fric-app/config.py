f6dd143ac9c8175be428d8712f114967526f28bc
Marco Soto <msoto9940@gmail.com>
2020-10-25 01:31:22 -0600

    Added launch script for app
    + Includes logging feature
    - Removed unused files

--- fric-app/config.py
+++ fric-app/config.py
@@ -2,16 +2,34 @@
 
 '''
 
-
-# Host IP for intializing servers
-HOST = 'localhost'
+# Default Host IP for intializing servers
+HOST = 'localhost' # 127.0.0.1
 
 # Ports for system database, backend, and frontend respectively
 MONGO_PORT = 27017
 NODE_PORT  = 5000
 REACT_PORT = 3000
 
-# Path to mongod config file
+# Path to mongod configuration file
 MONGO_CONF = './mongod.conf'
 
-__all__ = ['HOST', 'MONGO_PORT', 'NODE_PORT', 'REACT_PORT', 'MONGO_CONF']
+# Default database folder path where all bson files are stored
+DB_PATH = '/data/db'
+
+# Constants used for accessing database
+DB_NAME = 'fric_db'
+DB_USER = 'fricapp'
+DB_PASS = 'fricitup'
+
+def getConfigFileData():
+	"""This function parses the default set mongod.conf file for the path to the bson database and for the mongod port configuration."""
+	import re
+	global DB_PATH, MONGO_PORT
+	with open(MONGO_CONF, 'r') as file:
+		while line := file.readline():
+			pathMatch = re.search('(?<=dbPath: [\"\'])[\w\/]+', line)  # Get dbpath
+			portMatch = re.search('(?<=port: )\d+', line)  # Get port number
+			if pathMatch: DB_PATH = pathMatch.group(0)
+			if portMatch: MONGO_PORT = portMatch.group(0)
+
+getConfigFileData() # Get constants from mongod.conf
--- fric-app/launch.py
+++ fric-app/launch.py
@@ -1,24 +1,37 @@
 #! python3.8
 
-import os, subprocess, signal
-from config import *
+import os, sys, subprocess, signal
+import config  # Import config.py constants
 
-mongod_cmd = ['mongod', '-f', MONGO_CONF]
-node_cmd   = ['nodemon', './backend/server.js']
-react_cmd  = ['npm', 'start']
+# Command arguments for starting required system servers
+mongod_cmd = ['mongod', '--config', config.MONGO_CONF]
+backend_cmd   = ['node', './backend/server.js']
+frontend_cmd  = ['npm', 'start']
 
 def kill_required_ports():
-	outputs = [subprocess.getoutput(f'lsof -nti :{port}').split() for port in (MONGO_PORT, NODE_PORT, REACT_PORT)]
+	"""Used to kill processes int he ports required by the system."""
+	outputs = [subprocess.getoutput(f'lsof -nti :{port}').split() for port in (config.MONGO_PORT, config.NODE_PORT, config.REACT_PORT)]
 	for pids in outputs:
 		for pid in pids: os.kill(int(pid), signal.SIGKILL)
 
 def start_servers():
-	subprocess.Popen(mongod_cmd) # Start mongodb daemon
-	subprocess.Popen(node_cmd) # Start node backend server
-	subprocess.Popen(react_cmd) # Start react frontend server
+	"""Starts servers required to run the FRIC app."""
+	print('Starting FRIC App...')
+	print(f'MongoDB running at port: {config.MONGO_PORT}')
+	print(f'Node Backend running at port: {config.NODE_PORT}')
+	print(f'React Frontend running at port: {config.REACT_PORT}')
+
+	"""
+		The next block of code is for creating system logs. By default, the log files are overwritten (truncated) when this script is launched.
+		To change this behavior, alter the open file modifiers to 'a' instead of 'w'.
+	"""
+	if not os.path.isdir('./logs'): os.mkdir('./logs') # Make log directory
+	with open('logs/mongod.log', 'w') as mlog, open('logs/frontend.log', 'w') as nlog, open('logs/backend.log', 'w') as rlog: # Open log files for writing and truncate
+		subprocess.Popen(mongod_cmd, stdout=mlog) # Start mongodb daemon
+		subprocess.Popen(backend_cmd, stdout=nlog) # Start node backend server
+		subprocess.Popen(frontend_cmd, stdout=rlog) # Start react frontend server
 
 if __name__ == '__main__':
-	try:
-		kill_required_ports()
-		start_servers()
-	except Exception as err: print(err)
+	if '-d' in sys.argv: backend_cmd = ['nodemon', './backend/server.js'] # Set to development server
+	kill_required_ports()
+	start_servers()
