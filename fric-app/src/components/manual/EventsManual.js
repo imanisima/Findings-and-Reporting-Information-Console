78a09cccf08ef0848b0dd6f4ddf9658ac4cb3bdd
Imani A <imartin@miners.utep.edu>
2020-10-27 18:55:29 -0500

    added link to manual page

---
+++ fric-app/src/components/manual/EventsManual.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Container from '@material-ui/core/Container';
+
+
+class EventsManual extends Component {
+    render() {
+      return (
+        <Container maxWidth="sm">
+            Manual Here
+
+        </Container>
+
+      );
+    }
+}
+
+export default EventsManual
