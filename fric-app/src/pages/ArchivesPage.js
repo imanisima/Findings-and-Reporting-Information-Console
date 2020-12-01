cfc1b6397655fe044592cea255a4aa6527062bee
Imani <imanicaza@gmail.com>
2020-12-01 09:41:52 -0600

    Archive view (#26)
    * main page tooltips added
    
    * adding navbar tooltips
    
    * archive overview connected
    
    * added tooltips to archive
    
    * filling in archive tables
    
    * archive view

--- fric-app/src/pages/ArchivesPage.js
+++ fric-app/src/pages/ArchivesPage.js
@@ -5,15 +5,16 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import LayoutTemplate from '../components/general/LayoutTemplate';
-import OverviewCards from '../components/summary/OverviewCards';
+import ArchiveOverview from '../components/archive/FRIC_gui_archive_overview.js';
 import { darkTheme } from '../components/general/ThemeColors';
 
 export default function ArchivePage() {
+	
 	return (
 		// Added dark theme provider, remove for normal colors
 		<ThemeProvider theme={darkTheme}>
 			<LayoutTemplate
-				mainContentComponent={<OverviewCards />}
+				mainContentComponent={<ArchiveOverview />}
 			/>
 		</ThemeProvider>
 	);
